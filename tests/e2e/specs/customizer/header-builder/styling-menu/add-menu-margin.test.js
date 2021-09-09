0b995a5145d60deb0f59e7abb72cc5535a0e46a5
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-09-09 18:15:43 +0530

    added changes related to menu margin and menu spacing

--- tests/e2e/specs/customizer/header-builder/styling-menu/add-menu-margin.test.js
+++ tests/e2e/specs/customizer/header-builder/styling-menu/add-menu-margin.test.js
@@ -1,9 +1,11 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/set-customize';
-describe( 'Add menu spacing', () => {
-	it( 'menu margin should be added properly', async () => {
+describe( 'Add menu margin-left', () => {
+	it( 'menu margin-left should be added properly', async () => {
 		const menuMargin = {
-            'section-hb-menu-1-margin': '10px',
+            'section-hb-menu-1-margin': {
+				desktop:'10px',
+			}
 		};
 
 		await setCustomize( menuMargin );
@@ -16,10 +18,84 @@ describe( 'Add menu spacing', () => {
 
         await expect( {
 			selector: '#ast-hf-menu-1',
-			property: 'margin',
+			property: 'margin-left',
 		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
+			`${ menuMargin[ 'section-hb-menu-1-margin' ]}`,
 		);
 
 	} );
+	
+} );
+describe( 'Add menu margin-right', () => {
+	it( 'menu margin-right should be added properly', async () => {
+		const menuMargin = {
+            'section-hb-menu-1-margin': '20px',
+		};
+
+		await setCustomize( menuMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+
+        await expect( {
+			selector: '#ast-hf-menu-1',
+			property: 'margin-right',
+		} ).cssValueToBe(
+			`${ menuMargin[ 'section-hb-menu-1-margin' ]}`,
+		);
+
+	} );
+	
+} );
+describe( 'Add menu margin-top', () => {
+	it( 'menu margin-right should be added properly', async () => {
+		const menuMargin = {
+            'section-hb-menu-1-margin': '15px',
+		};
+
+		await setCustomize( menuMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+
+        await expect( {
+			selector: '#ast-hf-menu-1',
+			property: 'margin-top',
+		} ).cssValueToBe(
+			`${ menuMargin[ 'section-hb-menu-1-margin' ]}`,
+		);
+
+	} );
+	
+} );
+describe( 'Add menu margin-bottom', () => {
+	it( 'menu margin-bottom should be added properly', async () => {
+		const menuMargin = {
+            'section-hb-menu-1-margin': '25px',
+		};
+
+		await setCustomize( menuMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+
+        await expect( {
+			selector: '#ast-hf-menu-1',
+			property: 'margin-bottom',
+		} ).cssValueToBe(
+			`${ menuMargin[ 'section-hb-menu-1-margin' ]}`,
+		);
+
+
+	} );
+	
 } );
